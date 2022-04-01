73858ce8fba66ab27c8cf62c36c4c879e6c59e02
TheSeance <pauladanielle.celis@cvsu.edu.ph>
2022-04-01 12:20:26 +0800

    second commit

--- activity_02_Celis.py
+++ activity_02_Celis.py
@@ -1,9 +1,9 @@
 import random
 
-level = input("Enter level: ")
-power = input("Enter power: ")
-attack = input("Enter attack: ")
-defense = input("Enter defense: ")
+level = int(input("Enter level: "))
+power = int(input("Enter power: "))
+attack = int(input("Enter attack: "))
+defense = int(input("Enter defense: "))
 
 t=input("Enter no. of target: ")
 if(t==1):
@@ -19,7 +19,14 @@ elif(w==2):
 else:
     weather=1
 
-badge = 1
+b=str(input("Badge holder? y/n: "))
+checker="y"
+if(b==checker):
+    badge=1.25
+else:
+    badge=1
+
+
 crit = 1
 rfctr = round(random.uniform(0.85,1.0),2)
 stab = 1.5
@@ -31,5 +38,6 @@ modifier = target*weather*badge*crit*rfctr*stab*type*burn*1
 damage = ((2*level)/5)+2
 damage = (damage*power)*(attack/defense)
 damage = ((damage/50)+2)*modifier
+damage =  round(damage)
 
-print("Damage is: "+ round(damage))
+print("Damage is: ",damage)
